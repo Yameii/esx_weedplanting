98208a36d87c44ebba52adeeb586b254f70ee90a
depp <50743267+Zeedfy@users.noreply.github.com>
2019-07-16 01:02:15 +0700

    Add files via upload

---
+++ en.lua
@@ -0,0 +1,9 @@
+Locales["en"] = {
+  ["cancel"] = "Operation canceled.",
+  ["vehicle_deny"] = "Unfortunately, you can not be in the vehicle.",
+  ["wait_end"] = "Unfortunately, you must wait until you finish the previous action.",
+  ["wrong_place"] = "Unfortunately, you must wait until you finish the previous action.",
+  ["doing_action"] = "You are doing an operation. It takes about 40 seconds.",
+  ["how_cancel"] = "To cancel the operation click<button type='button'>X</button>",
+  ["wait_ten_sec"] = 'Wait at least 10 seconds before restarting this operation.',
+}
